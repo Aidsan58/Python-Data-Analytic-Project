fe1a2722c4fdd305a5670427ccf5547b72c695aa
Aidsan58 <aidanr1@comcast.net>
2025-12-23 12:54:38 -0800

    Created correlation function in analysis

--- src/analysis.py
+++ src/analysis.py
@@ -8,3 +8,8 @@ def basic_stats(df, column):
         "median": np.median(df[column]), # median
         "std": np.std(df[column]), # standard deviation
     }
+
+def correlation(df, column_1, column_2):
+    corr_value = df[column_1].corr(df[column_2])
+    print(f"Correlation between {column_1} and {column_2} is {corr_value}.")
+    return corr_value
