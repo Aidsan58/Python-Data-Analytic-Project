4ece42ba456f447fcea457ceddc049ca343eff12
Aidsan58 <aidanr1@comcast.net>
2025-12-23 12:56:35 -0800

    main now uses correlation from analysis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.data_loader import load_csv
 from src.data_cleaning import clean_data
-from src.analysis import basic_stats
+from src.analysis import basic_stats, correlation
 from src.visualization import plot_histogram, plot_scatterplot
 
 DATA_PATH = "data/data.csv" # change this line if your data is located elsewhere
@@ -14,7 +14,9 @@ def main():
 
     plot_histogram(df, df.columns[0])
 
-    plot_scatterplot(df, df['Yield (mg/mL)'], df['kcat (1/min)'])
+    plot_scatterplot(df, 'Yield (mg/mL)', 'kcat (1/min)')
+
+    correlation(df, 'Yield (mg/mL)', 'kcat (1/min)')
 
 if __name__ == "__main__":
     main()
