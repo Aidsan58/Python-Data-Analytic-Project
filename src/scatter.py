b212a23332852858da9d2e4d54fdc197579f9e89
Aidsan58 <aidanr1@comcast.net>
2025-12-22 18:32:04 -0800

    Delete rows that don't have numbers for Yield and kcat

--- src/scatter.py
+++ src/scatter.py
@@ -1,7 +1,25 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 pd.options.display.max_columns = 30
 
 df = pd.read_csv('../data/data.csv')
 
-print(df)
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+for x in df.index:
+    if is_number(df.loc[x, "Yield (mg/mL)"]) != True:
+        df.drop(x, inplace = True)
+
+for y in df.index:
+    if is_number(df.loc[y, "kcat (1/min)"]) != True:
+        df.drop(y, inplace = True)
+
+df.plot(kind = 'scatter', x = 'Yield (mg/mL)', y = 'kcat (1/min)')
+
+plt.show()
